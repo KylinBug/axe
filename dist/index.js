5b49e51adaefa6d1244c3c87c691515b4fe183df
Billy Chen <chenbufan@foxmail.com>
2023-07-21 00:17:47 +0800

    chore(dist): publish npm files

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calc = (a, b) => a - b;
+console.log(calc(5, 3));
+const add = (a, b) => a + b;
+exports.default = add;
+//# sourceMappingURL=index.js.map
